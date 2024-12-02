95ae77d02a16abcec1af7c57906f021e50b3150d
jwahoonkim <kjhoon0330@snu.ac.kr>
2024-12-03 03:12:26 +0900

    fx: UrlStorage 기능 추가

--- src/main/kotlin/me/jahni/urlshortener/infra/redis/RedisRepository.kt
+++ src/main/kotlin/me/jahni/urlshortener/infra/redis/RedisRepository.kt
@@ -2,31 +2,27 @@ package me.jahni.urlshortener.infra.redis
 
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
+import java.time.Duration
 
 @Component
 class RedisRepository(
     redisTemplate: RedisTemplate<String, String>
 ) {
     val ops = redisTemplate.opsForValue()
-    val setOps = redisTemplate.opsForSet()
 
     fun get(key: String): String? {
         return ops.get(key)
     }
 
-    fun set(key: String, value: String) {
-        ops.set(key, value)
+    fun set(
+        key: String,
+        value: String,
+        ttl: Duration = Duration.ofSeconds(60)
+    ) {
+        ops.set(key, value, ttl)
     }
 
     fun increment(key: String, delta: Long): Long {
         return ops.increment(key, delta)!!
-    }
-
-    fun isMember(key: String, value: String): Boolean {
-        return setOps.isMember(key, value)!!
-    }
-
-    fun add(key: String, value: String) {
-        setOps.add(key, value)
     }
 }
