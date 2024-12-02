571aea3aa2794dac80a9ac966de5b2153e547442
jwahoonkim <kjhoon0330@snu.ac.kr>
2024-12-03 01:27:35 +0900

    feat: UrlShortener 구현

---
+++ src/main/kotlin/me/jahni/urlshortner/component/UrlShortener.kt
@@ -0,0 +1,25 @@
+package me.jahni.urlshortner.component
+
+import org.springframework.stereotype.Component
+
+@Component
+class UrlShortener {
+
+    private val BASE62_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    fun shorten(id: Long): String {
+        val encoded = encode(id)
+        return encoded.toString().padStart(7, '0')
+    }
+
+    private fun encode(id: Long): StringBuilder {
+        var value = id
+        val encoded = StringBuilder()
+        while (value > 0) {
+            val index = (value % 62).toInt()
+            encoded.append(BASE62_CHARS[index])
+            value /= 62
+        }
+        return encoded
+    }
+}
